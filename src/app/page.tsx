7af092d382447eb12572da87892dbb6a76f3889f
Harrison-Blair <hjblair@me.com>
2025-09-30 22:43:16 -0400

    change width to 8xl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,26 +5,28 @@ import { Github, Linkedin, Mail, ChevronsDown } from 'lucide-react';
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col justify-between p-4"> {/* Flex layout with padding */}
-      <div className="flex flex-col justify-center items-start max-w-4xl"> {/* Center content vertically */}
-        <h1 className="text-7xl font-bold mb-12">
+      <div className="flex flex-col justify-center items-start"> {/* Center content vertically */}
+        <h1 className="text-8xl font-bold mb-12">
           Hi 👋,<br /> 
           I'm Harrison,
         </h1>
-        <p className="text-base md:text-lg mb-4 md:mb-6 text-[var(--secondary)]">
-          Recent Computer Science graduate with a strong foundation in the principles of iterative development, coding
-          best practices, modern cloud-based infrastructure, and complex problem solving. Proficient in languages such as
-          Python, Typescript, C++, and Rust. Experienced working in Agile, CI/CD and remote development teams...{" "}
-            <Link href="/work" className="text-[var(--primary)] hover:text-[var(--accent)] underline">
-              more
-            </Link>
-        </p>
-        <p className="text-base md:text-lg mb-4 md:mb-6 text-[var(--secondary)]">
-          In my free time, I enjoy hiking, photography, and exploring new technologies. Blah blah blah Blah blah blah 
-          Blah blah blah Blah blah Blah blah blah Blah blah blah blah Blah blah blah...{" "}
-            <Link href="/about" className="text-[var(--primary)] hover:text-[var(--accent)] underline">
-              more
-            </Link>
-        </p>
+        <div className="max-w-4xl">
+          <p className="text-base md:text-lg mb-4 md:mb-6 text-[var(--secondary)]">
+            Recent Computer Science graduate with a strong foundation in the principles of iterative development, coding
+            best practices, modern cloud-based infrastructure, and complex problem solving. Proficient in languages such as
+            Python, Typescript, C++, and Rust. Experienced working in Agile, CI/CD and remote development teams...{" "}
+              <Link href="/work" className="text-[var(--primary)] hover:text-[var(--accent)] underline">
+                more
+              </Link>
+          </p>
+          <p className="text-base md:text-lg mb-4 md:mb-6 text-[var(--secondary)]">
+            In my free time, I enjoy hiking, photography, and exploring new technologies. Blah blah blah Blah blah blah 
+            Blah blah blah Blah blah Blah blah blah Blah blah blah blah Blah blah blah...{" "}
+              <Link href="/about" className="text-[var(--primary)] hover:text-[var(--accent)] underline">
+                more
+              </Link>
+          </p>
+        </div>
         <div className="flex space-x-2 md:space-x-4 mt-4">
           <CircularButton
             href="https://github.com/Harrison-Blair"
