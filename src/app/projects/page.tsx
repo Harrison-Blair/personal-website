ac38a831d575b0e6cde3f4c040e31a2db7744492
Harrison-Blair <hjblair@me.com>
2025-10-06 21:55:11 -0400

    reformat projects tiling

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,13 +7,13 @@ export default function Projects() {
       <h1 className='text-[6.5vh]'>Projects</h1>
       <p className='text-[2vh]'>Here is a selection of my open source work.</p>
       <h2 className='text-[4vh] mt-4'>Pinned Projects</h2>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-8'>
+      <div className='grid grid-cols-3 gap-4 mb-8'>
         {projects.filter(project => project.pinned).map((project) => (
           <ProjectTile key={project.id} project={project} />
         ))}
       </div>
       <h2 className='text-[4vh] mt-4'>All Projects</h2>
-      <div className='grid grid-cols-3 gap-4'>
+      <div className='grid grid-cols-3 gap-4 mb-8'>
         {projects.map((project) => (
           <ProjectTile key={project.id} project={project} />
         ))}
