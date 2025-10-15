63f8576aba1e9dc3b8503e9efab4c77c53fea781
Harrison-Blair <hjblair@me.com>
2025-10-14 21:17:34 -0400

    fix(layout): adjust main margin for better layout responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${protoNF.variable} ${protoMonoNF.variable} antialiased`}>
         <Navigation />
-        <main className="ml-[var(--nav-width)] p-8">
+        <main className="mb-[var(--nav-height)] xl:ml-[var(--nav-width)] xl:mb-0 p-8">
           {children}
         </main>
       </body>
