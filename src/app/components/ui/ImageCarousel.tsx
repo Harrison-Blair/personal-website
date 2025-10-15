54187b84c6f3ae0097d6992132362aaa0e3b39f5
Harrison-Blair <hjblair@me.com>
2025-10-14 20:07:11 -0400

    add ImageCarousel Item to display images

---
+++ src/app/components/ui/ImageCarousel.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { useState } from "react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import Image from "next/image";
+
+export interface ImageCarouselProps {
+    imageUrls: string[];
+    boarder?: string;
+    styling?: string;
+    background?: string;
+}
+
+export default function ImageCarousel({
+    imageUrls,
+    boarder = '',
+    styling = '',
+    background = 'bg-slate-950',
+}: Readonly<ImageCarouselProps>) {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+
+    return (
+        <div className={`${styling} ${boarder} ${background}`}>
+            <Image
+                src={imageUrls[currentIndex]}
+                alt={`Image ${currentIndex + 1}`}
+                fill={true}
+                objectFit="contain"
+                className={styling}
+            />
+            <ChevronLeft
+                className="absolute left-0 top-1/2 transform -translate-y-1/2 cursor-pointer hover:text-[var(--accent)] p-2 transition-all duration-200"
+                size={'6vh'}
+                onClick={() => setCurrentIndex((currentIndex - 1 + imageUrls.length) % imageUrls.length)}
+            />
+            <ChevronRight
+                className="absolute right-0 top-1/2 transform -translate-y-1/2 cursor-pointer hover:text-[var(--accent)] p-2 transition-all duration-200"
+                size={'6vh'}
+                onClick={() => setCurrentIndex((currentIndex + 1) % imageUrls.length)}
+            />
+        </div>
+    );
+}
