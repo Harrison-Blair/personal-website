daa6a455c8789c71fee55001d114e8315564d4a3
Harrison-Blair <hjblair@me.com>
2025-10-14 21:17:27 -0400

    add mobile breakpoint for nav

--- src/app/components/layout/Navigation.tsx
+++ src/app/components/layout/Navigation.tsx
@@ -7,6 +7,7 @@ import { useState, useEffect } from 'react';
 
 export default function Navigation() {
     const ICON_SIZE = '3vw';
+    const MOBILE_ICON_SIZE = '5vh';
     const pathname = usePathname();
     const [isDark, setIsDark] = useState(false);
 
@@ -71,9 +72,29 @@ export default function Navigation() {
                     ))}
                 </ul>
             </nav>
-            <nav className="xl:hidden flex-row fixed left-0 bottom-0 w-full h-[var(--nav-height)] bg-[var(--muted)] py-4 z-50">
-
-            </nav>
+            <div className="xl:hidden h-full">
+                <button className="fixed top-4 right-4 bg-[var(--muted)] p-3 rounded-full" onClick={toggleTheme}>
+                    {isDark ? <MoonStar size={MOBILE_ICON_SIZE} /> : <Sun size={MOBILE_ICON_SIZE} />}
+                </button>
+                <nav className="flex-row fixed left-0 bottom-0 w-full h-[var(--nav-height)] bg-[var(--muted)] py-5 z-50">
+                    <ul className="flex flex-row justify-around">
+                    {topNavItems.map(({ href, icon: Icon, label }) => (
+                        <li key={href}>
+                            <Link
+                                href={href}
+                                title={label}
+                                className={`flex items-center justify-center mb-[2vw] transition-colors relative ${pathname === href
+                                        ? 'text-[var(--foreground)] border-b-[0.5vh] border-[var(--primary)]'
+                                        : 'text-[var(--secondary)] hover:text-[var(--accent)]'
+                                    }`}
+                            >
+                                <Icon size={MOBILE_ICON_SIZE} />
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+                </nav>
+            </div>
         </div>
     );
 }
