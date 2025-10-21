8e28f7d47ba8296abb07a50f59897ca0bdb1c395
Harrison-Blair <hjblair@me.com>
2025-10-21 19:53:01 -0400

    fix Tag component text size for improved readability

--- src/app/components/ui/Tag.tsx
+++ src/app/components/ui/Tag.tsx
@@ -3,7 +3,7 @@ export default function Tag({
     styling = ''
  }: { text: string, styling?: string }) {
     return (
-        <span className={`px-3 py-1 bg-[var(--primary)] text-[white] rounded-full text-[1.2vh] text-center ${styling}`}>
+        <span className={`px-3 py-1 bg-[var(--primary)] text-[white] rounded-full text-[0.9rem] text-center ${styling}`}>
             {text}
         </span>
     );
