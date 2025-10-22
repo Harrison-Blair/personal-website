4eba19b398ba88d6f09ed04f55aa2fe2de86f21e
Harrison-Blair <hjblair@me.com>
2025-10-21 22:56:53 -0400

    refactor: enhance Work component layout and improve content presentation

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -7,231 +7,112 @@ import { experiences } from "@/data/experiences";
 import Tag from "../components/ui/Tag";
 
 export default function Work() {
-    // Collect unique tags from all experiences
-    const uniqueTags = Array.from(
-        new Set(
-            experiences.flatMap(exp => exp.tags || [])
-        )
-    );
+  // Collect unique tags from all experiences
+  const uniqueTags = Array.from(
+    new Set(
+      experiences.flatMap(exp => exp.tags || [])
+    )
+  );
 
-    return (
-        <div>
-            <div className="hidden xl:block">
-                <h1 className="text-[3.5vw]">
-                    Harrison Blair
-                </h1>
-                <div className="grid grid-cols-[1fr_2fr]">
-                    <div> {/* Sidebar - 1/3 of Screen */}
-                        <div className="relative w-4/5 aspect-square m-2 mx-auto">
-                            <Image
-                                src="/images/Harrison-Blair.png"
-                                alt="Profile Picture"
-                                fill
-                                className="object-cover rounded-full border-[var(--primary)] border-4"
-                            />
-                        </div>
-                        <div className="flex flex-row gap-4 justify-center">
-                            <CircularButton
-                                href="https://github.com/Harrison-Blair"
-                                icon={Github}
-                                bgColor="bg-[var(--primary)]"
-                                hoverColor="hover:bg-[var(--accent)]"
-                                isExternal={true}
-                                size='8vh'
-                            />
-                            <CircularButton
-                                href="https://www.linkedin.com/in/harrison-blair/"
-                                icon={Linkedin}
-                                bgColor="bg-[var(--primary)]"
-                                hoverColor="hover:bg-[var(--accent)]"
-                                isExternal={true}
-                                size='8vh'
-                            />
-                            <CircularButton
-                                href="/contact"
-                                icon={Mail}
-                                bgColor="bg-[var(--primary)]"
-                                hoverColor="hover:bg-[var(--accent)]"
-                                isExternal={false}
-                                size='8vh'
-                            />
-                            <CircularButton
-                                href="/documents/Harrison-Blair-Web-Resume.pdf"
-                                icon={FileDown}
-                                bgColor="bg-[var(--primary)]"
-                                hoverColor="hover:bg-[var(--accent)]"
-                                isDownload={true}
-                                size='8vh'
-                            />
-                        </div>
-                        <div className="bg-[var(--muted)] rounded-[2.5rem] m-2 p-4 ">
-                            <div className="flex flex-col p-4 gap-2">
-                                <h2 className="text-[3vh] text-[var(--primary)]">
-                                    Education
-                                </h2>
-                                <a
-                                    href="https://www.champlain.edu"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    className="hover:text-[var(--accent)] text-[1.5vh] italic"
-                                >
-                                    Champlain College - Burlington, VT - Class of 2025
-                                </a>
-                                <p className="text-[2vh]">
-                                    B.S. in Computer Science, Minor in Cybersecurity
-                                </p>
-                            </div>
-                        </div>
-                        <div className="bg-[var(--muted)] rounded-[2.5rem] m-2 p-4 ">
-                            <div className="flex flex-col m-4 gap-2">
-                                <h2 className="text-[3vh] text-[var(--primary)]">
-                                    Skills
-                                </h2>
-                                <div className="flex flex-wrap gap-2">
-                                    {uniqueTags.map((tag, index) => (
-                                        <Tag key={index} text={tag} styling='px-3 py-2 text-[1.5vh]' />
-                                    ))}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div> {/* Main Content - 2/3 of Screen */}
-                        <div className="bg-[var(--muted)] rounded-[2.5rem] m-4 p-4 ">
-                            <div className="flex flex-col m-4">
-                                <p className="text-[2vh]">
-                                    Recent Computer Science graduate with a strong foundation in the principles of iterative development, coding
-                                    best practices, modern cloud-based infrastructure, and complex problem solving. Proficient in languages such as
-                                    Python, Typescript, C++, and Rust. Experienced working in Agile, CI/CD and remote development teams.
-                                </p>
-                            </div>
-                        </div>
-                        <div className="m-4 p-4 ">
-                            <h2 className="text-[4vh] text-[var(--primary)]">
-                                Experience
-                            </h2>
-                            <hr className="border-[var(--primary)] border-[1.5px]" />
-                            <div className="flex flex-col">
-                                {experiences.map((experience, index) => (
-                                    <div key={experience.id}>
-                                        <ExperienceTile
-                                            experience={experience}
-                                        />
-                                        {index < experiences.length - 1 && (
-                                            <div className="h-8 w-1 bg-[var(--primary)] mx-auto -mt-4 -mb-4"></div>
-                                        )}
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className='p-4'>
+      <h1>
+        Experience
+      </h1>
+      <div className='grid grid-cols-1 gap-4
+                      lg:grid-cols-[1fr_2fr]'>
+        <div className='flex flex-col items-center'>
+          <div className='relative w-2/3 aspect-square mb-4'>
+            <Image
+              src="/images/Harrison-Blair.png"
+              alt="Profile Picture"
+              fill
+              className="object-cover rounded-full border-[var(--primary)] border-4"
+            />
+          </div>
+          <div className="flex justify-center space-x-4
+                              lg:justify-start">
+            <CircularButton
+              href="https://github.com/Harrison-Blair"
+              icon={Github}
+              bgColor="bg-[var(--primary)]"
+              hoverColor="hover:bg-[var(--accent)]"
+              isExternal={true}
+            />
+            <CircularButton
+              href="https://www.linkedin.com/in/harrison-blair/"
+              icon={Linkedin}
+              bgColor="bg-[var(--primary)]"
+              hoverColor="hover:bg-[var(--accent)]"
+              isExternal={true}
+            />
+            <CircularButton
+              href="/contact"
+              icon={Mail}
+              bgColor="bg-[var(--primary)]"
+              hoverColor="hover:bg-[var(--accent)]"
+            />
+            <CircularButton
+              href="/documents/Harrison-Blair-Web-Resume.pdf"
+              icon={FileDown}
+              bgColor="bg-[var(--primary)]"
+              hoverColor="hover:bg-[var(--accent)]"
+              isDownload={true}
+            />
+          </div>
+          <div className='w-[85%] bg-[var(--muted)] rounded-3xl text-center p-4 m-4
+                          lg:text-left'>
+            <h2>
+              Education
+            </h2>
+            <a
+              href="https://www.champlain.edu"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-[var(--accent)] text-[0.9rem] italic"
+            >
+              Champlain College - Burlington, VT
+            </a>
+            <p className="text-[1.1rem] mt-2">
+              Bachelors of Science in Computer Science, <br />
+              Minor in Cybersecurity
+            </p>
+          </div>
+          <div className='w-[85%] bg-[var(--muted)] rounded-3xl text-center p-4 m-4
+                          lg:text-left'>
+            <h2>
+              Skills
+            </h2>
+            <div className="flex flex-wrap gap-2">
+              {uniqueTags.map((tag, index) => (
+                <Tag key={index} text={tag} styling='px-3 py-2 text-[1.5vh]' />
+              ))}
             </div>
-            <div className="xl:hidden">
-                <div className="flex flex-col items-center">
-                    <h1 className="text-[8vw] text-center">
-                        Harrison Blair
-                    </h1>
-                    <div className="relative w-full aspect-square m-2 mx-auto">
-                        <Image
-                            src="/images/Harrison-Blair.png"
-                            alt="Profile Picture"
-                            fill
-                            className="object-cover rounded-full border-[var(--primary)] border-4 "
-                        />
-                    </div>
-                    <div className="flex flex-row gap-4 justify-center">
-                        <CircularButton
-                            href="https://github.com/Harrison-Blair"
-                            icon={Github}
-                            bgColor="bg-[var(--primary)]"
-                            hoverColor="hover:bg-[var(--accent)]"
-                            isExternal={true}
-                            size='8vh'
-                        />
-                        <CircularButton
-                            href="https://www.linkedin.com/in/harrison-blair/"
-                            icon={Linkedin}
-                            bgColor="bg-[var(--primary)]"
-                            hoverColor="hover:bg-[var(--accent)]"
-                            isExternal={true}
-                            size='8vh'
-                        />
-                        <CircularButton
-                            href="/contact"
-                            icon={Mail}
-                            bgColor="bg-[var(--primary)]"
-                            hoverColor="hover:bg-[var(--accent)]"
-                            isExternal={false}
-                            size='8vh'
-                        />
-                        <CircularButton
-                            href="/documents/Harrison-Blair-Web-Resume.pdf"
-                            icon={FileDown}
-                            bgColor="bg-[var(--primary)]"
-                            hoverColor="hover:bg-[var(--accent)]"
-                            isDownload={true}
-                            size='8vh'
-                        />
-                    </div>
-                    <div className="bg-[var(--muted)] rounded-[2.5rem] mt-4 p-4">
-                        <div className="flex flex-col gap-2">
-                            <h2 className="text-[3vh] text-[var(--primary)]">
-                                Education
-                            </h2>
-                            <a
-                                href="https://www.champlain.edu"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="hover:text-[var(--accent)] text-[1.5vh] italic"
-                            >
-                                Champlain College - Burlington, VT - Class of 2025
-                            </a>
-                            <p className="text-[2vh]">
-                                B.S. in Computer Science, Minor in Cybersecurity
-                            </p>
-                        </div>
-                    </div>
-                    <div className="bg-[var(--muted)] rounded-[2.5rem] mt-4 p-4 ">
-                        <div className="flex flex-col text-center">
-                            <p className="text-[2vh]">
-                                Recent Computer Science graduate with a strong foundation in the principles of iterative development, coding
-                                best practices, modern cloud-based infrastructure, and complex problem solving. Proficient in languages such as
-                                Python, Typescript, C++, and Rust. Experienced working in Agile, CI/CD and remote development teams.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="mt-4">
-                        <h2 className="text-[4vh] text-[var(--primary)]">
-                            Experience
-                        </h2>
-                        <hr className="border-[var(--primary)] border-[1.5px]" />
-                        <div className="flex flex-col">
-                            {experiences.map((experience, index) => (
-                                <div key={experience.id}>
-                                    <ExperienceTile
-                                        experience={experience}
-                                    />
-                                    {index < experiences.length - 1 && (
-                                        <div className="h-8 w-1 bg-[var(--primary)] mx-auto -mt-4 -mb-4"></div>
-                                    )}
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                    <div className="bg-[var(--muted)] rounded-[2.5rem] mt-2 p-4">
-                        <div className="flex flex-col m-4 gap-2">
-                            <h2 className="text-[3vh] text-[var(--primary)]">
-                                Skills
-                            </h2>
-                            <div className="flex flex-wrap gap-2">
-                                {uniqueTags.map((tag, index) => (
-                                    <Tag key={index} text={tag} styling='px-3 py-2 text-[1.5vh]' />
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
-    );
+        <div className='text-center
+                        lg:text-start'>
+          <p className='bg-[var(--muted)] rounded-3xl p-4 mb-4
+                        lg:text-[1.2rem]'>
+            Hi, my name is Harrison. I recently graduated from Champlain College with a B.S. in Computer Science and minor in Cybersecurity. Over the past few years I&apos;ve built strong full stack software development skills, using languages like Python, Typescript, C++, and Rust. I am experienced working in agile software development teams, implementing continuous integration/continuous delivery pipelines, modern cloud-based architecture and remote development teams. I love solving complex problems with code, no matter the domain.
+          </p>
+          <h2 className="m-4">
+            Work Experience
+          </h2>
+          <div className="flex flex-col">
+            {experiences.map((experience, index) => (
+              <div key={experience.id}>
+                <ExperienceTile
+                  experience={experience}
+                />
+                {index < experiences.length - 1 && (
+                  <div className="h-[2rem] w-[0.25rem] bg-[var(--primary)] mx-auto -mt-4 -mb-4"></div>
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
